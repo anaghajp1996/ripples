00392970b40cfe41d31b336a796061245e055753
Anagha <anaghajayaprakash1996@gmail.com>
2023-11-22 10:35:22 -0600

    feat: Attach ripple to tap position

--- ripples/ContentView.swift
+++ ripples/ContentView.swift
@@ -7,20 +7,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State var innerRipple = Ripple(diameter: 0, color: Color.init(red: 87/255, green: 75/255, blue:144/255), number: 5)
+extension CGPoint : Hashable {
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(x)
+    hasher.combine(y)
+  }
+}
 
+struct ContentView: View {
+    var innerRipple = Ripple(diameter: 0, color: Color.init(red: 87/255, green: 75/255, blue:144/255), number: 5)
+    @State var positions: [CGPoint] = []
     var body: some View {
         ZStack {
             Rectangle()
                 .fill(.clear)
-            RippleView(ripple: innerRipple)
-        }
-        .onAppear {
-            withAnimation(Animation.linear(duration: 3.0).repeatForever(autoreverses: false)) {
-                innerRipple.diameter = 300
+                .contentShape(Rectangle())
+                .onTapGesture(coordinateSpace: .global) { location in
+                    positions.append(location)
+                }
+            ForEach(positions, id: \.self) { position in
+                RippleView(ripple: innerRipple).position(position)
+                    
             }
-            
         }
         .edgesIgnoringSafeArea(.all)
     }
@@ -33,7 +41,7 @@ struct Ripple {
 }
 
 struct RippleView: View {
-    let ripple: Ripple
+    @State var ripple: Ripple
 
     var body: some View {
         ForEach(1...ripple.number, id: \.self) { i in
@@ -41,7 +49,11 @@ struct RippleView: View {
                 .stroke(ripple.color.opacity(Double(ripple.number - i)/10.0), lineWidth: CGFloat(i*30))
                 .fill(ripple.color.opacity(Double(i)/10.0))
                 .frame(width: ripple.diameter, height: ripple.diameter)
-                
+                .onAppear {
+                    withAnimation(.linear(duration: 3.0)) {
+                        ripple.diameter = 100
+                    }
+                }
         }
     }
 }
