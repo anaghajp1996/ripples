b4a2e52d8713de86eac43d596706f15503ce1569
Anagha <anaghajayaprakash1996@gmail.com>
2023-11-22 17:47:08 -0600

    refactor: Improve ripples

--- ripples/ContentView.swift
+++ ripples/ContentView.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+let colors: [Color] = [
+    Color.init(red: 87/255, green: 75/255, blue:144/255),
+    Color.init(red: 119/255, green: 139/255, blue:235/255),
+    Color.init(red: 247/255, green:215/255, blue:148/255),
+    Color.init(red: 207/255, green: 106/255, blue:135/255),
+]
+
 extension CGPoint : Hashable {
   public func hash(into hasher: inout Hasher) {
     hasher.combine(x)
@@ -15,7 +22,7 @@ extension CGPoint : Hashable {
 }
 
 struct ContentView: View {
-    var ripple = Ripple(diameter: 0, color: Color.init(red: 87/255, green: 75/255, blue:144/255), number: 5, ripple: false)
+    @State var ripple = Ripple(diameter: 0, color: colors.randomElement()!, lineWidth: 10)
     @State var positions: [CGPoint] = []
     var body: some View {
         ZStack {
@@ -23,11 +30,11 @@ struct ContentView: View {
                 .fill(.clear)
                 .contentShape(Rectangle())
                 .onTapGesture(coordinateSpace: .global) { location in
+                    ripple.color = colors.randomElement()!
                     positions.append(location)
                 }
             ForEach(positions, id: \.self) { position in
                 RippleView(ripple: ripple).position(position)
-                    
             }
         }
         .edgesIgnoringSafeArea(.all)
@@ -37,21 +44,21 @@ struct ContentView: View {
 struct Ripple {
     var diameter: CGFloat
     var color: Color
-    var number: Int
-    var ripple: Bool
+    var lineWidth: CGFloat
 }
 
 struct RippleView: View {
     @State var ripple: Ripple
+    let number = 5
     var body: some View {
-        ForEach(1...ripple.number, id: \.self) { i in
+        ForEach(1...number, id: \.self) { i in
             Circle()
-                .stroke(ripple.color.opacity(Double(ripple.number - i)/10.0), lineWidth: ripple.ripple ? 0 : CGFloat(i*10))
+                .stroke(ripple.color.opacity(Double(number - i)/10.0), lineWidth: ripple.lineWidth * Double(i))
                 .frame(width: ripple.diameter, height: ripple.diameter)
                 .onAppear {
-                    withAnimation(.linear(duration: 3.0)) {
-                        ripple.diameter = 100
-                        ripple.ripple.toggle()
+                    withAnimation(.linear(duration: 1.5)) {
+                        ripple.diameter = 150
+                        ripple.lineWidth = 0
                     }
                 }
         }
