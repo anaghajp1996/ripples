5f1b105bcc96f96a7daf641c3335abf5a93d4547
Anagha <anaghajayaprakash1996@gmail.com>
2023-11-21 18:56:50 -0600

    Initial Commit

---
+++ ripples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ripples
+//
+//  Created by Anagha K J on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ripples/ripplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ripplesApp.swift
+//  ripples
+//
+//  Created by Anagha K J on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ripplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
